d99d7fa6f078dc8dcdf318dc2fcf78290d0173ca
Questorkonan <sergigullam@gmail.com>
2019-04-15 16:05:19 +0300

    2 files modified

--- src/app/shared/torrivic.service.ts
+++ src/app/shared/torrivic.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs';
+import { Task } from "../shared/task";
 
 @Injectable()
 export class TorrivicService {
@@ -46,7 +46,37 @@ getUserbyId = function(id : number){
 
 // Task Manager
 
-  tasks : Task[];
+  tasks : Task[] = [
+    {
+  id: 1,
+  name: "full-range",
+  description: "T1-T6 level with complete lesion of spinal cord"
+}, {
+  id: 2,
+  name: "budgetary management",
+  description: "Letterer-siwe disease, lymph nodes of head, face, and neck"
+}, {
+  id: 3,
+  name: "Mandatory",
+  description: "Closed fracture of unspecified bone(s) of foot [except toes]"
+}, {
+  id: 4,
+  name: "foreground",
+  description: "Accidental poisoning by agents primarily affecting gastrointestinal system"
+}, {
+  id: 5,
+  name: "algorithm",
+  description: "Closed fracture of T1-T6 level with central cord syndrome"
+}
+  ];
+
+  getTasks(){
+    return this.tasks;
+  }
+
+  addTask(entity : Task){
+    this.tasks.push(entity);
+  }
 
 
 }
--- src/app/task-manager/task-manager.component.ts
+++ src/app/task-manager/task-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TorrivicService } from './../shared/torrivic.service';
 
 @Component({
   selector: 'app-task-manager',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskManagerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _ts: TorrivicService) { }
 
   ngOnInit() {
+    console.log(this._ts.getTasks())
   }
 
 }
