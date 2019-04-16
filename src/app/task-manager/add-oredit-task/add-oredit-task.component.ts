ef5d0ff39cbf024386c577237764eb57a543a80e
Questorkonan <sergigullam@gmail.com>
2019-04-16 16:09:09 +0300

    2 files modified

--- src/app/task-manager/add-oredit-task/add-oredit-task.component.ts
+++ src/app/task-manager/add-oredit-task/add-oredit-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TorrivicService } from '../../shared/torrivic.service';
+import { Task } from "../../shared/task";
 
 @Component({
   selector: 'app-add-oredit-task',
@@ -11,30 +12,28 @@ export class AddOreditTaskComponent implements OnInit {
 
   taskId: string;
   buttonText: string;
-
+  task : Task;
 
   constructor(private route: ActivatedRoute , private _ts: TorrivicService) {
 
-    this.taskId = this.route.snapshot.params['id'];
-  
-    if (this.taskId == undefined) {
-      this.buttonText = 'Add Task';
-
-      
-
-     
-    } else {
-      this.buttonText = 'Edit Task';
-
-console.log(this._ts.getSingleTask(parseInt(this.taskId)))
-
-    }
+    
 
   }
 
   ngOnInit() {
 
+this.taskId = this.route.snapshot.params['id'];
+  
+    console.log(this.taskId)
 
+    if (this.taskId == undefined || this.taskId == null || this.taskId == "") {
+      this.buttonText = 'Add Task';
+    } else {
+      this.buttonText = 'Edit Task';
+      this.task =  this._ts.getSingleTask(parseInt(this.taskId));
+    }
+
+     
 
 
   }
