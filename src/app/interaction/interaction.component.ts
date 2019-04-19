57edecb280c012cbd01288c39ec44c933a5c5d5f
Questorkonan <sergigullam@gmail.com>
2019-04-19 14:05:41 +0300

    1 file modified

--- src/app/interaction/interaction.component.ts
+++ src/app/interaction/interaction.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit  , ViewChild , ElementRef , AfterViewInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-interaction',
   templateUrl: './interaction.component.html',
   styleUrls: ['./interaction.component.css']
 })
-export class InteractionComponent implements OnInit , AfterViewInit{
+export class InteractionComponent implements OnInit, AfterViewInit {
 
   messageFromParent: string = "This message send from parent to child"
   messageFromChild: string = "";
@@ -13,34 +13,50 @@ export class InteractionComponent implements OnInit , AfterViewInit{
   @ViewChild("childelementRef") childelementRef: ElementRef;
   @ViewChild("elementRefUsage") elementRefUsage: ElementRef;
 
-  constructor() { }
+  constructor(private _render: Renderer2) { }
 
   displayMessage(event) {
     this.messageFromChild = event;
   }
 
   ngOnInit() {
-  
+
   }
 
   ngAfterViewInit() {
-     this.childelementRef.nativeElement.focus();
-     this.childelementRef.nativeElement.value =  5 ;
+    this.childelementRef.nativeElement.focus();
+    this.childelementRef.nativeElement.value = 5;
   }
 
-bindchildelementRef(){
-  alert(this.childelementRef.nativeElement.value)
+  bindchildelementRef() {
 
-if(this.childelementRef.nativeElement.value >= 5){
-  this.elementRefUsage.nativeElement.innerText = 'More than 5';
-  this.elementRefUsage.nativeElement.style.color = 'blue';
-}else{
-    this.elementRefUsage.nativeElement.innerText = 'Less than 5';
-  this.elementRefUsage.nativeElement.style.color = 'red';
-}
+    if (this.childelementRef.nativeElement.value >= 5) {
+
+      // Stage 1
+      // this.elementRefUsage.nativeElement.innerText = 'More than 5';
+      // this.elementRefUsage.nativeElement.style.color = 'blue';
 
 
+      // OR
+      this._render.setProperty(this.elementRefUsage.nativeElement, 'innerHTML', 'More than 5');
+      this._render.addClass(this.elementRefUsage.nativeElement, 'text-success');
 
-}
+
+    } else {
+
+      // Stage 1
+      // this.elementRefUsage.nativeElement.innerText = 'Less than 5';
+      // this.elementRefUsage.nativeElement.style.color = 'red';
+
+      // OR
+      this._render.setProperty(this.elementRefUsage.nativeElement, 'innerHTML', 'Less than 5');
+      this._render.addClass(this.elementRefUsage.nativeElement, 'text-primary');
+
+
+    }
+
+
+
+  }
 
 }
